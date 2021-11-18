514e290798704adb57d0b91fd215fd597975100b
Nazanin Shahrad <W0766771@myscc.ca>
2021-11-18 13:48:06 -0500

    add Customer Area to Area, remove all errors,addproject reference to main project

--- ShahBookStore/Controllers/HomeController.cs
+++ ShahBookStore/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using ShahBookStore.Models;
+using ShahBookStore.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
