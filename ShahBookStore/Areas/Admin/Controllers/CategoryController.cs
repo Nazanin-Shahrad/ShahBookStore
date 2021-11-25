61ceee0628db3af01ad8b07fdbefdd32097aaaef
Nazanin Shahrad <W0766771@myscc.ca>
2021-11-25 11:51:18 -0500

    all Upsert.cshtml in Area/Views/Category and add content from provided document

--- ShahBookStore/Areas/Admin/Controllers/CategoryController.cs
+++ ShahBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShahsBooks.DataAccess.Repository.IRepository;
+using ShahsBooks.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,11 +21,28 @@ namespace ShahBookStore.Areas.Admin.Controllers
         {
             return View();
         }
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+            if (id == null)
+            {
+                return View(category);
+            }
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if (category == null)
+            {
+                return NotFound();
+
+            }
+            return View();
+        }
 
         public IActionResult GetAll()
         {
             var allObj = _unitOfWork.Category.GetAll();
             return Json(new { data = allObj });
         }
+
+        
     }
 }
