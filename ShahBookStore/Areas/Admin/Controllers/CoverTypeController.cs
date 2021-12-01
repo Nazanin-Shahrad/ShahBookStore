08662fda9782fa6adebd0bb1686a28e8bb7ea1cb
Nazanin Shahrad <W0766771@myscc.ca>
2021-12-01 18:03:17 -0500

    add CoverTypeController

---
+++ ShahBookStore/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShahsBooks.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShahBookStore.Areas.Admin.Controllers
+{
+    public class CoverTypeController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            var allObj =_unitOfWork.CoverType.GetAll();
+            return Json(new { data = allObj });
+        }
+    }
+}
--- ShahsBooks.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ ShahsBooks.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace ShahsBooks.DataAccess.Repository.IRepository
      public interface IUnitOfWork : IDisposable
     {
         ICategoryRepository Category { get; }
+        ICoverTypeRepository CoverType { get; }
         ISP_Call SP_Call { get; }
 
         public void Save();
